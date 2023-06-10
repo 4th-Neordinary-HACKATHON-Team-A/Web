8da258e8db356afd3a912ef2be56934ddaeb98da
alreadynyeong <kjw87130@naver.com>
2023-06-11 01:18:10 +0900

    Feat: 채팅창 하단 input과 디자인 바 생성

---
+++ assets/svg/ChatBarIcon.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import Svg, { Circle } from 'react-native-svg';
+
+export default function ChatBarIcon(props) {
+  return (
+    <Svg width="23" height="11" viewBox="0 0 23 11" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <Circle cx="5.5" cy="5.5" r="5.5" fill="#9B84F7"/>
+        <Circle cx="17.2334" cy="5.5" r="5.5" fill="#9B84F7"/>
+        <Circle cx="11.3667" cy="5.5" r="5.5" fill="#9B84F7"/>
+    </Svg>
+
+  );
+}
