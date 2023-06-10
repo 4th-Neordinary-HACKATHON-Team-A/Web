dc9b6ee56363b1daf1a6ba46b87b7bc903b3890f
alreadynyeong <kjw87130@naver.com>
2023-06-10 18:21:21 +0900

    Feat: Chat 컴포넌트 생성

---
+++ components/Chat.tsx
@@ -0,0 +1,46 @@
+import { View, Text, StyleSheet } from "react-native";
+import COLORS from "../styles/colors";
+
+interface ChatProps {
+    text?: string;   
+}
+
+export const MyChat = ({text}: ChatProps) => {
+    return (
+        <View style={styles.myBox}>
+            <Text style={styles.myText}>{text}</Text>
+        </View>
+    )
+}
+export const SystemChat = ({text}: ChatProps) => {
+    return (
+        <View style={styles.systemBox}>
+            <Text style={styles.systemText}>{text}</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    myBox: {
+        backgroundColor: '#FFF',
+        paddingHorizontal: 16,
+        paddingVertical: 8,
+        borderRadius: 20,
+    },
+    myText: {
+        color: '#000',
+        fontSize: 14,
+        fontWeight: '600',
+    },
+    systemBox: {
+        backgroundColor: COLORS.PURPLE_100,
+        paddingHorizontal: 16,
+        paddingVertical: 8,
+        borderRadius: 20,
+    },
+    systemText: {
+        color: '#FFF',
+        fontSize: 14,
+        fontWeight: '600',
+    }
+})
