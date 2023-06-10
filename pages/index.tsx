a47013ce6ceb0cf113ac0711fbf81f571a44df45
devryyeong <thisisme20@daum.net>
2023-06-11 04:54:59 +0900

    Chore: 임시 코드 삭제

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,6 @@ import {createStackNavigator} from '@react-navigation/stack'
 import {StyleSheet, Text, View} from 'react-native'
 import {commonStyles} from '../styles/common'
 import Home from './Home/Home'
-import Post from './Post'
 
 const Stack = createStackNavigator()
 
@@ -12,7 +11,6 @@ const Pages = () => {
     <View>
       {/* <Stack.Screen name='home' component={Home} /> */}
       {/* <Home /> */}
-      <Post />
     </View>
   )
 }
