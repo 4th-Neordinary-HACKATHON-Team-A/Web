3a5cd33d52b7b883b7c0002cc1660463acf271fc
alreadynyeong <kjw87130@naver.com>
2023-06-11 05:07:45 +0900

    Feat: Stack 화면 이동 설정

--- App.tsx
+++ App.tsx
@@ -5,6 +5,14 @@ import useRememoryFonts from './infrastructures/hooks/useRememoryFonts'
 import Pages from './pages'
 import {commonStyles} from './styles/common'
 import LoadingAnimation from './components/LoadingAnimation'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+
+import OnBoarding from './pages/onBoarding'
+import Home from './pages/Home/Home'
+import ChatPage from './pages/Chat/ChatPage'
+import COLORS from './styles/colors'
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   const {fontsLoaded} = useRememoryFonts()
@@ -14,9 +22,40 @@ export default function App() {
   }
 
   return (
-    <SafeAreaProvider style={commonStyles.container}>
-      <Pages />
-      {/* <LoadingAnimation /> */}
-    </SafeAreaProvider>
+    
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{
+         headerStyle: {
+          backgroundColor: COLORS.BG_BLACK,
+        },
+        headerTintColor: 'white',
+      }} initialRouteName="OnBoarding">
+          <Stack.Screen
+            name="OnBoarding"
+            component={OnBoarding}
+            options={{
+              headerShown: false,
+            }}
+          />
+          <Stack.Screen
+            name="Home"
+            component={Home}
+            options={{
+              headerTitle: ''
+            }}
+          />
+          <Stack.Screen
+            name="Chat"
+            component={ChatPage}
+            options={{
+              headerTitle: ''
+            }}
+          />
+           </Stack.Navigator>
+    </NavigationContainer>
+    // <SafeAreaProvider style={commonStyles.container}>
+    //   <Pages />
+    // </SafeAreaProvider>
   )
 }
+
