9d848e80224501e140aad36832f292187a76b8eb
박지수 <jisupark2000@gmail.com>
2023-06-11 04:18:37 +0900

    Feat: 업로드 화면 UI 완성

---
+++ pages/Upload/Upload.styled.tsx
@@ -0,0 +1,57 @@
+import styled from 'styled-components/native'
+
+export const Container = styled.View`
+  padding: 0 10px;
+`
+
+export const Title = styled.Text`
+  margin-bottom: 24px;
+  font-size: 20px;
+  font-weight: 600;
+  color: #fff;
+`
+
+export const SubTitle = styled.Text`
+  margin-left: 2px;
+  margin-bottom: 10px;
+  font-size: 12px;
+  font-weight: 700;
+  color: #fff;
+`
+
+export const GeneratedImg = styled.Image`
+  width: calc(100% - 72px);
+`
+
+export const MonologCategories = styled.View`
+  display: flex;
+  flex-direction: row;
+  gap: 40px;
+  flex-wrap: wrap;
+  margin-bottom: 24px;
+`
+
+export const SubTitleWithSubText = styled.Text`
+  font-size: 12px;
+  font-weight: 700;
+  color: #fff;
+`
+
+export const SubTitleSubText = styled.Text`
+  margin-left: 2px;
+  font-size: 12px;
+  font-weight: 400;
+  color: #fff;
+`
+
+export const InputBox = styled.View`
+  display: flex;
+  justify-content: center;
+`
+
+export const TextInput = styled.TextInput`
+  padding: 6px;
+  font-size: 14px;
+  font-weight: 400;
+  color: #fff;
+`
