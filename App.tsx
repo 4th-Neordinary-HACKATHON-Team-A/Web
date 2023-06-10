ae664ddfe177dac0f0d8ebe8a31184c8bf378d50
박지수 <jisupark2000@gmail.com>
2023-06-11 08:38:15 +0900

    Feat: Splash 화면 완성

--- App.tsx
+++ App.tsx
@@ -1,12 +1,11 @@
 import {Text} from 'react-native'
-import * as SplashScreen from 'expo-splash-screen'
-import { SafeAreaProvider } from 'react-native-safe-area-context'
+import {SafeAreaProvider} from 'react-native-safe-area-context'
 import {NavigationContainer} from '@react-navigation/native'
 import useRememoryFonts from './infrastructures/hooks/useRememoryFonts'
 import Pages from './pages'
 import {commonStyles} from './styles/common'
 import LoadingAnimation from './components/LoadingAnimation'
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import {createNativeStackNavigator} from '@react-navigation/native-stack'
 
 import OnBoarding from './pages/onBoarding'
 import Home from './pages/Home/Home'
@@ -15,7 +14,8 @@ import Categories from './pages/Categories/Categories'
 import Result from './pages/Result'
 import Upload from './pages/Upload/Upload'
 import COLORS from './styles/colors'
-const Stack = createNativeStackNavigator();
+import SplashScreen from './pages/SplashScreen'
+const Stack = createNativeStackNavigator()
 
 export default function App() {
   const {fontsLoaded} = useRememoryFonts()
@@ -26,59 +26,68 @@ export default function App() {
 
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{
-         headerStyle: {
-          backgroundColor: COLORS.BG_BLACK,
-        },
-        headerTintColor: 'white',
-      }} initialRouteName="OnBoarding">
-          <Stack.Screen
-            name="OnBoarding"
-            component={OnBoarding}
-            options={{
-              headerShown: false,
-            }}
-          />
-          <Stack.Screen
-            name="Home"
-            component={Home}
-            options={{
-              headerTitle: ''
-            }}
-          />
-          <Stack.Screen
-            name="Chat"
-            component={ChatPage}
-            options={{
-              headerTitle: ''
-            }}
-          />
-           <Stack.Screen
-            name="Category"
-            component={Categories}
-            options={{
-              headerTitle: ''
-            }}
-          />
-           <Stack.Screen
-            name="Result"
-            component={Result}
-            options={{
-              headerTitle: ''
-            }}
-          />
-          <Stack.Screen
-            name="Upload"
-            component={Upload}
-            options={{
-              headerTitle: ''
-            }}
-          />
-           </Stack.Navigator>
+      <Stack.Navigator
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: COLORS.BG_BLACK,
+          },
+          headerTintColor: 'white',
+        }}
+        initialRouteName='x'
+      >
+        <Stack.Screen
+          name='SplashScreen'
+          component={SplashScreen}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name='OnBoarding'
+          component={OnBoarding}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name='Home'
+          component={Home}
+          options={{
+            headerTitle: '',
+          }}
+        />
+        <Stack.Screen
+          name='Chat'
+          component={ChatPage}
+          options={{
+            headerTitle: '',
+          }}
+        />
+        <Stack.Screen
+          name='Category'
+          component={Categories}
+          options={{
+            headerTitle: '',
+          }}
+        />
+        <Stack.Screen
+          name='Result'
+          component={Result}
+          options={{
+            headerTitle: '',
+          }}
+        />
+        <Stack.Screen
+          name='Upload'
+          component={Upload}
+          options={{
+            headerTitle: '',
+          }}
+        />
+      </Stack.Navigator>
     </NavigationContainer>
     // <SafeAreaProvider style={commonStyles.container}>
     //   <Pages />
     // </SafeAreaProvider>
   )
 }
-
