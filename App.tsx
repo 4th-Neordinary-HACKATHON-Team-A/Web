d25817d2461bae4797cdef1f537f8e4aa14d15f6
devryyeong <thisisme20@daum.net>
2023-06-10 17:13:46 +0900

    Style: font 적용

--- App.tsx
+++ App.tsx
@@ -1,9 +1,17 @@
 import {StyleSheet, Text, View} from 'react-native'
+import useRememoryFonts from "./infrastructures/hooks/useRememoryFonts"
 
 export default function App() {
+  const { fontsLoaded } = useRememoryFonts();
+
+  if (!fontsLoaded) {
+    return <Text>Loading...</Text>
+  }
+
   return (
     <View style={styles.container}>
-      <Text>ㅇㅇ</Text>
+      <Text style={styles.font}>폰트체크</Text>
+      <Text style={styles.font}>Font Check 123 </Text>
     </View>
   )
 }
@@ -15,4 +23,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  font: {
+    fontFamily: 'Pretendard-Bold',
+    fontSize: 14,
+  }
 })
