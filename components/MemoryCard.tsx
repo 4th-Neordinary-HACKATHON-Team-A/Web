439dc9f45fbebf925af3e6c6ea49950be2ba844e
devryyeong <thisisme20@daum.net>
2023-06-11 10:19:44 +0900

    Feat: 실시간 업로드 컴포넌트

--- components/MemoryCard.tsx
+++ components/MemoryCard.tsx
@@ -1,17 +1,28 @@
 import React from 'react'
-import { TouchableOpacity, Text, StyleSheet, View, Platform, ImageBackground } from 'react-native'
-
+import { TouchableOpacity, Text, StyleSheet, View, Image, ImageBackground } from 'react-native'
+import Img1Src from '../assets/img/memories/img-1.png'
+import Img2Src from '../assets/img/memories/img-2.png'
+import Img3Src from '../assets/img/memories/img-3.png'
+import Img4Src from '../assets/img/memories/img-4.png'
+import Img5Src from '../assets/img/memories/img-5.png'
+import Img6Src from '../assets/img/memories/img-6.png'
+import Img7Src from '../assets/img/memories/img-7.png'
+import Img8Src from '../assets/img/memories/img-8.png'
+import Img9Src from '../assets/img/memories/img-9.png'
+import Img10Src from '../assets/img/memories/img-10.png'
+import { memoryData } from "./../assets/memory";
 interface CardProps {
   title?: string;
   name: string;
   time?: number;
-  source: string;
+  uri?: string;
 }
-const MemoryCard = ({ title, name, time }: CardProps) => {
+
+const MemoryCard = ({ title, name, time, uri }: CardProps) => {
   return (
     <View style={styles.container}>
       <View style={styles.cardBox}>
-        <ImageBackground source={require('../assets/image-temp.jpg')} resizeMode='cover' style={styles.card}>
+        <ImageBackground source={uri} resizeMode='cover' style={styles.card}>
           <View style={styles.textBox}>
             <Text style={styles.mainText}>{title}</Text>
             <Text style={styles.subText}>by {name}</Text>
@@ -27,6 +38,7 @@ const MemoryCard = ({ title, name, time }: CardProps) => {
 
 const styles = StyleSheet.create({
   container: {
+    display:'flex',
     justifyContent: 'center',
     flexDirection: 'row',
   },
@@ -34,14 +46,16 @@ const styles = StyleSheet.create({
     borderWidth: 3,
     borderStyle: 'solid',
     borderColor: 'rgba(255, 255, 255, 0.3)',
-    marginHorizontal: 10,
+    marginHorizontal: 5,
+    marginBottom: 5,
+    // backgroundColor:'red'
   },
   card: {
     display: 'flex',
     flexDirection: 'column-reverse',
     backgroundColor: 'yellow',
-    width: 144,
-    height: 144,
+    width: 160,
+    height: 160,
   },
   textBox: {
     backgroundColor: 'white',
